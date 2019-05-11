2ec18c49bcbfc9bae533de1e962adb914c585940
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-05-10 21:56:44 -0500

    created a loop that creates list items with underscores and added click event to submit button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 var userGuessButton = document.getElementById("user-guess-button");
+var wrongGuess = 0;
+var randomWord = words[Math.floor(Math.random()*words.length)];
+var blanks = document.getElementById("blanks");
+console.log(randomWord)
+
+for(i = 0; i < randomWord.length; i++){
+    var newLi = document.createElement("li");
+    blanks.appendChild(newLi);
+    newLi.textContent = "_";
+}
 
 
-console.log(words)
-// userGuessButton.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     var userGuess = document.getElementById("user-guess").value;
-//     console.log(userGuess);
+userGuessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    var userGuess = document.getElementById("user-guess").value;
+    console.log(userGuess);
 
-    
-// });
+    if(randomWord.includes(userGuess)){
+        
+    }else{
+        wrongGuess++;
+    }
+});
+
+
+
+
+
+
+
+
+// play again button to refresh page
