4ac824888f1ef7b18aa6177d3484a9d9d5e57cc4
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-05-11 17:47:41 -0500

    all JavaScript for hangman game working. Styling Needed

--- js/script.js
+++ js/script.js
@@ -1,25 +1,72 @@
 var userGuessButton = document.getElementById("user-guess-button");
 var wrongGuess = 0;
-var randomWord = words[Math.floor(Math.random()*words.length)];
+var correctGuess = 0;
+var randomWord = words[Math.floor(Math.random() * words.length)];
 var blanks = document.getElementById("blanks");
+var gameOver = document.getElementById("game-over")
 console.log(randomWord)
 
-for(i = 0; i < randomWord.length; i++){
+for (var i = 0; i < randomWord.length; i++) {
     var newLi = document.createElement("li");
     blanks.appendChild(newLi);
     newLi.textContent = "_";
 }
 
 
+
 userGuessButton.addEventListener("click", function (e) {
     e.preventDefault();
     var userGuess = document.getElementById("user-guess").value;
-    console.log(userGuess);
 
-    if(randomWord.includes(userGuess)){
-        
-    }else{
+    if (randomWord.includes(userGuess)) {
+
+        for (var i = 0; i < randomWord.length; i++) {
+            if (userGuess === randomWord.charAt(i)) {
+                console.log(randomWord.charAt(i));
+                console.log(blanks.childNodes[0]);
+                blanks.childNodes[i + 1].textContent = randomWord.charAt(i);
+                // append child here
+            }
+
+            correctGuess++
+
+        }
+    } else {
         wrongGuess++;
+    }
+
+    if (wrongGuess === 1) {
+        document.getElementById("base").style.display = "block";
+    } else if (wrongGuess === 2) {
+        document.getElementById("stand").style.display = "block";
+    } else if (wrongGuess === 3) {
+        document.getElementById("top").style.display = "block";
+    } else if (wrongGuess === 4) {
+        document.getElementById("hanger").style.display = "block";
+    } else if (wrongGuess === 5) {
+        document.getElementById("head").style.display = "block";
+    } else if (wrongGuess === 6) {
+        document.getElementById("body").style.display = "block";
+    } else if (wrongGuess === 7) {
+        document.getElementById("left-arm").style.display = "block";
+    } else if (wrongGuess === 8) {
+        document.getElementById("right-arm").style.display = "block";
+    } else if (wrongGuess === 9) {
+        document.getElementById("left-leg").style.display = "block";
+    } else if (wrongGuess === 10) {
+        document.getElementById("right-leg").style.display = "block";
+    }
+
+
+
+    if (wrongGuess === 10) {
+        
+        gameOver.style.display = "block";
+        gameOver.childNodes[0].textContent = "You Lose!";
+        console.log("WORKING")
+    } else if (correctGuess === randomWord.length) {
+        gameOver.childNodes[0].textContent("You Win!");
+        gameOver.style.display = "block";
     }
 });
 
