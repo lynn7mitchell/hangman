c50a8bd3790ffdf4501af41ed579ccbd1019abfc
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-05-10 21:15:26 -0500

    html, some css, and images added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var userGuessButton = document.getElementById("user-guess-button");
+
+
+console.log(words)
+// userGuessButton.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     var userGuess = document.getElementById("user-guess").value;
+//     console.log(userGuess);
+
+    
+// });
